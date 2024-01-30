921044cbc8c39f1fcd3986952286c1496f3c77b2
JithinGeorge10 <jithin.george.el@gmail.com>
2024-01-30 14:30:29 +0530

    commit

--- script.js
+++ script.js
@@ -9,26 +9,60 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick= () => {
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
 
 //Contact-Me form submission
 
-$("#submit-form").submit((e)=>{
-    e.preventDefault()
-    $.ajax({
-        url:"https://script.google.com/macros/s/AKfycbxQK-BwNklb8-5ojajplhwbnqwWS0uloEUBH1lioGuGw8x01i-7-6BDHt2rmwG9pj_vAw/exec",
-        data:$("#submit-form").serialize(),
-        method:"post",
-        success:function (response){
-            alert("Form submitted successfully")
-            window.location.reload()
-            //window.location.href="https://google.com"
-        },  
-        error:function (err){
-            alert("Something Error")
+// $("#submit-form").submit((e) => {
+//     e.preventDefault()
+//     $.ajax({
+//         url: "https://script.google.com/macros/s/AKfycbxQK-BwNklb8-5ojajplhwbnqwWS0uloEUBH1lioGuGw8x01i-7-6BDHt2rmwG9pj_vAw/exec",
+//         data: $("#submit-form").serialize(),
+//         method: "post",
+//         success: function (response) {
+//             alert("Form submitted successfully")
+//             window.location.reload()
+//             //window.location.href="https://google.com"
+//         },
+//         error: function (err) {
+//             alert("Something Error")
+//         }
+//     })
+// })
+
+
+$("#submit-form").validate({
+    rules: {
+        username: {
+            pattern: /^[a-zA-Z ]+$/
+        },
+        phone: {
+            pattern: /^\+?[1-9][0-9]{9}$/
         }
-    })
+
+    },
+    submitHandler: function (form) {
+
+
+        $.ajax({
+            url: "https://script.google.com/macros/s/AKfycbxQK-BwNklb8-5ojajplhwbnqwWS0uloEUBH1lioGuGw8x01i-7-6BDHt2rmwG9pj_vAw/exec",
+            data: $(form).serialize(),
+            method: "post",
+            success: function (response) {
+                alert("Form submitted successfully")
+                form.reset();
+                window.location.reload()
+
+                //window.location.href="https://google.com"
+            },
+            error: function (err) {
+                alert("Something Error")
+            }
+        })
+
+    }
 })
+
